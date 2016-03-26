3897583a98673717c3ff27a96f7722f48884d04e
lights li <lightsever@hotmail.com>
2016-03-26 17:55:33 +0800

    add

---
+++ unityproj/Assets/editor/webunity.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Diagnostics;
+
+public class WebUnityWindow : EditorWindow
+{
+    [MenuItem("WebUnity/ShowToolBar", false, 13)]
+    private static void ShowWin()
+    {
+        var window = (WebUnityWindow)EditorWindow.GetWindow(typeof(WebUnityWindow), false, "Web ToolBar");
+        window.Init();
+    }
+    string[] outline;
+    string codepath = Application.dataPath + "/tscode";
+    void Build()
+    {
+        Process p = new Process();
+        p.StartInfo = new ProcessStartInfo(codepath +"/build.bat");
+        p.StartInfo.WorkingDirectory = codepath;
+        p.StartInfo.CreateNoWindow = true;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        p.StartInfo.RedirectStandardOutput = true;
+        p.Start();
+        p.WaitForExit();
+        string txt = p.StandardOutput.ReadToEnd();
+        outline = txt.Split(new char[] { '\n' }, System.StringSplitOptions.None);
+
+    }
+    
+    void RunHtml()
+    {
+        string path = Application.dataPath + "/.html/index.html";
+        //UnityEngine.Debug.LogWarning("html path=" + path);
+        EditorUtility.OpenWithDefaultApp(path);
+    }
+    Vector2 outlinepos = Vector2.zero;
+    Vector2 codepos = Vector2.zero;
+    public void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Build And Run"))
+            {
+                Build();
+                RunHtml();
+            }
+            if (GUILayout.Button("Build Html Only"))
+            {
+                Build();
+            }
+            if (GUILayout.Button("Run Html Only"))
+            {
+                RunHtml();
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(4);
+        GUILayout.Label("CodeFile Here.");
+
+        var files = System.IO.Directory.GetFiles(codepath, "*.ts");
+
+        codepos = GUILayout.BeginScrollView(codepos);
+        foreach (var f in files)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(f.Substring(codepath.Length+1));
+            if (GUILayout.Button("edit"))
+            {
+                EditorUtility.OpenWithDefaultApp(f);
+            }
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndScrollView();
+
+        GUILayout.Space(4);
+        GUILayout.Label("LogInfo Here.");
+        outlinepos = GUILayout.BeginScrollView(outlinepos);
+        if (outline != null)
+        {
+            for (int i = 0; i < outline.Length; i++)
+            {
+                GUILayout.Label("out:" + outline[i]);
+            }
+        }
+        GUILayout.EndScrollView();
+    }
+    public void Init()
+    {
+
+    }
+}
